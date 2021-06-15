7a366dc27bed098086d824d2a50b544b4666dac0
stormshadow3000 <83730643+stormshadow3000@users.noreply.github.com>
2021-06-15 11:52:34 +0530

    Update and rename gbam.py to gayban.py

--- userbot/plugins/gayban.py
+++ userbot/plugins/gayban.py
@@ -8,7 +8,7 @@ from userbot.utils import edit_or_reply
 from . import *
 
 
-@catub.cat_cmd(pattern=r"gbam$")
+@catub.cat_cmd(pattern=r"gayban$")
 async def _(event):
     reply_message = await event.get_reply_message()
     username = reply_message.sender.first_name
@@ -28,7 +28,7 @@ async def _(event):
         await event.edit(f"#Gbanned \n{username} \nfrom spam watch \nfrom chutiya watch \nfrom Randwa watch \nchats affected :- 43958 ")
      
     if h==2:
-         await event.edit(f"Gbanning {username} from all sudo u have ")
+        await event.edit(f"Gbanning {username} from all sudo u have ")
         await asyncio.sleep(3)
         await event.edit(f"Gbanning  This gay {username} ")
         await asyncio.sleep(3)
