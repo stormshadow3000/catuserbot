1633ecff54c59bcab16b5d202cca433a72695728
stormshadow3000 <83730643+stormshadow3000@users.noreply.github.com>
2021-06-15 11:46:25 +0530

    Create gbam.py

---
+++ userbot/plugins/gbam.py
@@ -0,0 +1,41 @@
+#fake
+from telethon import events
+import asyncio
+import os
+import sys
+import random
+from userbot.utils import edit_or_reply
+from . import *
+
+
+@catub.cat_cmd(pattern=r"gbam$")
+async def _(event):
+    reply_message = await event.get_reply_message()
+    username = reply_message.sender.first_name
+    if event.fwd_from:
+        return
+    await event.edit("gbanning This fumcker ass.... \nfrom telegram")
+    await asyncio.sleep(3)
+    h=(random.randrange(1,4))
+    if h==1:
+        
+        k = await event.get_reply_message()
+        username = k.sender.first_name
+        await event.edit(f"Gbanning {username} from all sudo u have ")
+        await asyncio.sleep(3)
+        await event.edit(f"Gbanning  This gay {username} ")
+        await asyncio.sleep(3)
+        await event.edit(f"#Gbanned \n{username} \nfrom spam watch \nfrom chutiya watch \nfrom Randwa watch \nchats affected :- 43958 ")
+     
+    if h==2:
+         await event.edit(f"Gbanning {username} from all sudo u have ")
+        await asyncio.sleep(3)
+        await event.edit(f"Gbanning  This gay {username} ")
+        await asyncio.sleep(3)
+        await event.edit(f"#Gbanned \n{username} \nfrom spam watch \nfrom chutiya watch \nfrom Randwa watch  \nchats affected :- 43958 ")
+    if h==3:
+        await event.edit(f"Gbanning {username} from all sudo u have ")
+        await asyncio.sleep(3)
+        await event.edit(f"Gbanning  This gay {username} ")
+        await asyncio.sleep(3)
+        await event.edit(f"#Gbanned \n{username} \nfrom spam watch \nfrom chutiya watch \nfrom Randwa watch  \nchats affected :- 43958 ")
